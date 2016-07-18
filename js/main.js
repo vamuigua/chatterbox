729de3ea69c3d1bf09db5cf30f867716696d9bb9
Victor Allen <vamuigua@gmail.com>
2016-07-18 16:03:17 +0300

    added click function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function mySend(){
-    document.getElementById("message-display").innerHTML = "Hello World!";
+function mySend() {
+    document.getElementById("message").value = document.getElementById("write").value;
 }
+
+/*$(document).ready(function(){
+   var message = $('#write').val();
+    
+   $('#btn').on('click',function(){
+       $('#message-display').append('<p>'+message+'</p>');
+   });
+});*/
